3da8c57b79ffc02084c6924f72c32aad80ca95d8
Aditya Prabowo <adityaprabowo.dev@gmail.com>
2024-08-07 19:19:50 +0700

    Learn about arithmetic operators

---
+++ Python/Fundamentals/Kelas Terbuka/operator-aritmatika/main.py
@@ -0,0 +1,26 @@
+x = 10
+y = 3
+
+# penambahan
+print("Hasil penambahan:", x + y)
+
+# pengurangan
+print("Hasil pengurangan:", x - y)
+
+# pembagian
+print("Hasil pembagian:", x / y)
+
+# perkalian
+print("Hasil perkalian:", x * y)
+
+# modulo
+print("Hasil modulo:", x % y)
+
+# eksponen
+print("Hasil eksponen:", x ** y)
+
+# floor division
+print("Hasil floor division:", x // y)
+
+# operation precedence
+print(5 + (20 % 3) / 4 + 90 - 2 // 3 ** 2 * 2)
