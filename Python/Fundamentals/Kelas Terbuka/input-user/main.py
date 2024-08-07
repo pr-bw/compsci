2c4d85ebfab84e2803fe37393fe30a3665bef58e
Aditya Prabowo <adityaprabowo.dev@gmail.com>
2024-08-07 18:40:41 +0700

    Learn about user input

---
+++ Python/Fundamentals/Kelas Terbuka/input-user/main.py
@@ -0,0 +1,8 @@
+email = input("Masukan email anda: ")
+nama_lengkap = input("Masukan nama lengkap anda: ")
+umur = int(input("masukan umur anda: "))
+
+print("email:", email, "bertipe:", type(email))
+print("nama lengkap:", nama_lengkap, "bertipe:", type(nama_lengkap))
+print("umur:", umur, "bertipe:", type(umur))
+
