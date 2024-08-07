90e730f16cf2c1564750792f4406caa3361509dd
Aditya Prabowo <adityaprabowo.dev@gmail.com>
2024-08-07 13:38:11 +0700

    Initial commit

---
+++ Python/Fundamentals/Kelas Terbuka/cara-kerja/main.py
@@ -0,0 +1,20 @@
+import time
+
+start_time = time.time()
+
+print("Start everything with bismillah.")
+
+# ini adalah comment
+
+"""
+Ini adalah multi-line comment
+untuk comment multi baris
+"""
+
+for i in range(1,5000):
+    y = 3
+
+x = 20
+print(x)
+
+print("finish time:", time.time() - start_time, "detik")
