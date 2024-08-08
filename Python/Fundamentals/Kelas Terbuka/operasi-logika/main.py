f3a9a28130ccb351d1af7a115eb89d8e993979f3
Aditya Prabowo <adityaprabowo.dev@gmail.com>
2024-08-08 11:19:40 +0700

    Learn about logic or boolean operations

---
+++ Python/Fundamentals/Kelas Terbuka/operasi-logika/main.py
@@ -0,0 +1,25 @@
+# 1.) or
+print("1.======== or =======")
+print(True or False)
+print(False or True)
+print(False or False)
+print(True or True)
+
+# 2.) and
+print("2.======== and =======")
+print(True and False)
+print(False and True)
+print(False and False)
+print(True and True)
+
+# 3.) not
+print("3.======= not ========")
+print(not False)
+print(not True)
+
+# 4.) xor
+print("4.======= xor ========")
+print(False ^ True)
+print(True ^ False)
+print(False ^ False)
+print(True ^ True)
