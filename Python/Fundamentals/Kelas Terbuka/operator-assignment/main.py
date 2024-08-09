f66b3fcf5a953cd1fc35d96a7a4eb5e356a780a7
Aditya Prabowo <adityaprabowo.dev@gmail.com>
2024-08-09 21:17:58 +0700

    Learn about assignment operators

---
+++ Python/Fundamentals/Kelas Terbuka/operator-assignment/main.py
@@ -0,0 +1,55 @@
+# penjelasan: merupakan unary operation untuk satu variable itu sendiri
+
+x = 2
+print(x)
+
+# penjumlahan
+x+=2
+print(x)
+
+# pengurangan
+x-=2
+print(x)
+
+# perkalian
+x*=50
+print(x)
+
+# pembagian
+x/=10
+print(x)
+
+# eksponensial
+x**=2
+print(x)
+
+# floor division
+x//=3
+print(x)
+
+# bitwise or
+a = True
+a |= False
+print(a)
+
+# bitwise and
+a = False
+a &= True
+print(a)
+
+# bitwise xor
+a = True
+a ^= True
+print(a)
+
+# bitwise shift left
+x = 12
+print(format(x, '08b'))
+x<<=2
+print(format(x, '08b'))
+
+# bitwise shift right
+y = 24
+print(format(y, '08b'))
+y>>=3
+print(format(y, '08b'))
